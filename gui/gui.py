57f0b0a00607d3aa122f37302bdcf69861182fcf
Ash <ashrec23@gmail.com>
2026-02-20 12:36:01 +0000

    switing to using a ui file for easier alterations

--- gui/gui.py
+++ gui/gui.py
@@ -1,36 +1,18 @@
 import asyncio
 import json
 import ctypes
-
-from qasync import QEventLoop, asyncSlot
-
-from PyQt6.QtCore import QSize, Qt
+from qasync import asyncSlot
 from PyQt6.QtGui import QIcon
-from PyQt6.QtWidgets import QApplication, QMainWindow, QWidget, QVBoxLayout, QHBoxLayout, QLabel, QPushButton, QListWidget, QScrollArea
+from PyQt6.QtWidgets import QMainWindow, QLabel, QScrollArea
+from PyQt6 import uic
 
 from core.sensors import DepthSensor, Anemometer, VesselSpeed
 from core.scheduler import Scheduler
 
-
 #~~ Global Constants
 APPLICATION_NAME = "NMEA 2000 Sim"
 ICON_PATH = 'gui/resources/icon.ico'
-WINDOW_HEIGHT = 450
-WINDOW_WIDTH = 1200
-LABEL_HEIGHT = 15
-SCROLL_BAR_WIDTH = 10
-
-SIM_WIDTH = int((WINDOW_WIDTH-100)/3)  # Width of the sim column
-LOG_WIDTH = int(2*(WINDOW_WIDTH-100)/3)  # Width of the log column
-BUTTON_HEIGHT = int((2 * WINDOW_HEIGHT)/9)
-BUTTON_WIDTH = int((WINDOW_WIDTH-100)/6)
-OPTIONS_HEIGHT = int((5 * WINDOW_HEIGHT)/9)
-LOG_HEIGHT = int((7 * WINDOW_HEIGHT)/9)
-
-BUTTON_NAMES = ["Start", "Stop"]
-SIMULATION_LABEL_TITLE = "Simulation Modes:"
-LOG_LABEL_TITLE = "Data Log:"
-
+WINDOW_PATH = 'gui/resources/mainwindow.ui'
 #~~
 
 # Subclass QMainWindow to customise the window
@@ -39,86 +21,30 @@ class MainWindow(QMainWindow):
     def __init__(self, condition_list, loop=None):
         super().__init__()
 
-        self.mode_chosen = None
+        uic.loadUi(WINDOW_PATH, self)   # loads window as defined in mainwindow.ui file
 
-        # setup app window
+        # setup app window and icon
         self.setWindowTitle(APPLICATION_NAME)
-        self.setFixedSize(QSize(WINDOW_WIDTH, WINDOW_HEIGHT))
-        self.setWindowIcon(QIcon(ICON_PATH)) # set icon in window
-        ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID('nmea-sim.gui') # set icon in taskbar
-        # 
+        # self.setFixedSize(self.size())
+        self.setWindowIcon(QIcon(ICON_PATH))
+        ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID('nmea-sim.gui')
 
-        self.sim_running = False # TODO: make redundant
+        # wire up widgets from .ui
+        self.modeListWidget.addItems(condition_list)
+        self.modeListWidget.currentItemChanged.connect(self.ModeChosen)
+        self.startButton.clicked.connect(self.PlayButtonClicked)     # start
+        self.stopButton.clicked.connect(self.StopButtonClicked)   # stop
 
-        # three widgets for app window
-        layout_main = QHBoxLayout()
-        layout_left = QVBoxLayout()
-        layout_right = QVBoxLayout()
-        #
+        # log label lives inside the scroll area
+        self.log_label: QLabel = self.findChild(QLabel, "logLabel")
+        self.log_label.setText("")  # start empty
 
-        # sim mode widgets (left)
-        layout_left.addWidget(self.AddLabel(SIMULATION_LABEL_TITLE, SIM_WIDTH, LABEL_HEIGHT))
-        layout_left.addWidget(self.AddListWidget(condition_list ,SIM_WIDTH, OPTIONS_HEIGHT))
-        layout_left.addLayout(self.BuildButtonRow())
-
-        layout_main.addLayout(layout_left)
-        #
-
-        # data log widgets (right)
-        layout_right.addWidget(self.AddLabel(LOG_LABEL_TITLE, LOG_WIDTH, LABEL_HEIGHT))
-
-        self.log_label = self.AddLabel("", LOG_WIDTH)
-
-        scroll = QScrollArea() # scroll bar for data log
-        scroll.setHorizontalScrollBarPolicy(Qt.ScrollBarPolicy.ScrollBarAlwaysOff)
-        scroll.setWidgetResizable(True)
-        scroll.setWidget(self.log_label)
-        layout_right.addWidget(scroll)
-        #
-        layout_main.addLayout(layout_right)
-
-        widget = QWidget()
-        widget.setLayout(layout_main)
-        self.setCentralWidget(widget)
-
-        
+        self.mode_chosen = None
+        self.sim_running = False
         self.loop = loop or asyncio.get_event_loop()
         self.log_queue = asyncio.Queue()
         self.loop.create_task(self.UpdateLogLabel())
         self.scheduler = None
-
-    def BuildButtonRow(self):
-        layout_buttons = QHBoxLayout()
-        count = 0
-        for title in BUTTON_NAMES:
-            button = QPushButton(title)
-            button.setFixedSize(BUTTON_WIDTH, BUTTON_HEIGHT)
-
-            if count == 0: # play
-                button.clicked.connect(self.PlayButtonClicked)
-            elif count == 1: # pause
-                button.clicked.connect(self.StopButtonClicked)
-            else: # restart
-                button.clicked.connect(self.RestartButtonClicked)
-
-            layout_buttons.addWidget(button)
-            count += 1
-        return layout_buttons
-
-    def AddLabel(self, text, width, height=None):
-        label = QLabel(text)
-        if height:
-            label.setFixedSize(width, height)
-        else:
-            label.setFixedWidth(LOG_WIDTH)
-        return label
-
-    def AddListWidget(self, list_options, width, height):
-        widget = QListWidget()
-        widget.addItems(list_options)
-        widget.setFixedSize(width, height)
-        widget.currentItemChanged.connect(self.ModeChosen)
-        return widget
 
     def ModeChosen(self, m):
         self.mode_chosen = m.text()
@@ -159,6 +85,13 @@ class MainWindow(QMainWindow):
         while True:
             message = await self.log_queue.get()
             self.log_label.setText((self.log_label.text() + "\n" + message).strip())
+            self.log_label.adjustSize()  # let label report its new height
+            self.log_label.parent().adjustSize()  # keep scroll widget in sync
+            
+            self.scrollArea.verticalScrollBar().setValue(
+                self.scrollArea.verticalScrollBar().maximum()
+            )
+            
             self.log_queue.task_done()
     
     def ResetLogLabel(self):
